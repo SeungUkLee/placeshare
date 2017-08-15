39ca5388b1eb49220c63083cd065364ad157331a
Ubuntu <ubuntu@ip-172-31-24-131.ap-northeast-2.compute.internal>
2017-08-16 04:46:31 +0900

    make toggle menu and searchPlaces

--- resources/views/placeposts/index.blade.php
+++ resources/views/placeposts/index.blade.php
@@ -3,20 +3,108 @@
 @section('content')
     @php $viewName = 'placeposts.index'; @endphp
     <div class="row">
-        <div id="map" class="col-md-9">
+        <div id="map-wrap" class="col-md-9">
+            <div id="map"></div>
+            <div id="menu-wrap">
+                <form>
+                    <input class="list-toggle" id="list-toggle" type="checkbox" hidden>
+                    <label for="list-toggle" id="listIcon">
+                        <span class="listIcon-bar"></span>
+                    </label>
+                </form>
+
+                <div id="searchPlaces-warp">
+                    <input type="text" id="searchKeyword" size="25" placeholder="장소, 주소...">
+                    <button type="submit" onclick="searchPlaces()">검색</button>
+                </div>
+                <ul id="placeList"></ul>
+                <div id"paginationa"></div>
+            </div>
         </div>
 <!-- =====TODO 직접 추가 바람start-->
         @section('style')
-        <style>
-#map {
-  height:60vh;        
+<style>
+#map-wrap {
+  height: 60vh;        
+  font-family: 'Malgun Gothic',dotum, '돋움',sans-serif;
+  font-size: 12px;
 }
+
+#map-wrap a, #map-wrap a:hover, #map-wrap a:active{
+  color: #000;
+  text-decoration: none;
+}
+
+#menu-wrap {
+  position: absolute;
+  top: 5px;left: 20px;
+  min-height: 35px;
+  max-height: 80%;
+  width: 250px;
+  padding 5px;
+  overflow-y: auto;
+  background:rgba(255, 255, 255, 0.6);
+  z-index: 1;
+  border-radius: 10px;
+}
+
+#map {
+  height: 100%;
+  /*position:relative;*/
+  /*overflow:hidden;*/
+}
+
+.listIcon-bar {
+  display: block;
+  width: 20px;
+  height: 3px;
+  background-color: #333;
+  position: relative;
+  transition: background-color .2s ease-out;
+}
+
+.listIcon-bar::before, .listIcon-bar::after {
+  display: block;
+  background-color: #333;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  content: "";
+}
+
+.listIcon-bar::before {
+  top: -7px;
+}
+.listIcon-bar::after{
+  top: 7px;
+}
+
+#listIcon {
+  float: left;
+  cursor: pointer;
+  margin: 5px;
+  padding: 10px 0px;
+
+  -webkit-user-select: none;  /* Chrome all / Safari all */
+  -moz-user-select: none;     /* Firefox all */
+  -ms-user-select: none;      /* IE 10+ */
+  user-select: none;          /* Likely future */
+}
+
+#searchPlaces-warp {
+  margin: 5px;
+  float:right;
+  /*position: absolute;
+  top: 3px;
+  left: 30px;*/
+}
+
 @media only screen and (min-width : 720px) { /*col-md 대응*/
-  #map {
-    height:80vh;        
+  #map-wrap {
+    height: 80vh;        
   }
 }
-        </style>
+</style>
         @stop
 
         @section('script')
